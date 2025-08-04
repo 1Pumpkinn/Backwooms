e02e14bbb51aa1de70f489893ad0a21b114a07f8
1Pumpkinn <wishsmp1002@gmail.com>
2025-08-05 00:27:41 +1000

    t3rr

---
+++ src/main/java/net/tyrone/backwooms/Backwooms.java
@@ -0,0 +1,10 @@
+package net.tyrone.backwooms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Backwooms implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/tyrone/backwooms/client/BackwoomsClient.java
@@ -0,0 +1,10 @@
+package net.tyrone.backwooms.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BackwoomsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
